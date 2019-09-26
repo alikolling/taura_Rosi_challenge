ecc4da2841d79bebb81c2e64a50bb1e8660067ae
Victor Augusto Kich <victorkich@yahoo.com.br>
2019-09-26 17:14:02 -0300

    lala

--- scripts/test_kinect_depth.py
+++ scripts/test_kinect_depth.py
@@ -7,28 +7,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sensor_msgs.msg import Image 
 from cv_bridge import CvBridge, CvBridgeError
-
+#from sensor_msgs.msg import Float32
 
 class Image_kinect_depth():
     def __init__(self):
         self.sub = rospy.Subscriber("sensor/kinect_depth", Image, self.callback, queue_size=1)
-        self.bridge = CvBridge()
-       
+        
+        #self.pub = rospy.Publisher("/rosi/command_kinect_joint", Float32, queue_size=10)
+        self.pub = rospy.Publisher("depth/image", Image, queue_size=10)
+
     def callback(self, ros_data):
+        bridge = CvBridge()
+        cv_image = bridge.imgmsg_to_cv2(ros_data, "bgr8")
 
-
-        cv_image = self.bridge.imgmsg_to_cv2(ros_data, desired_encoding="passthrough")
-        
-        
-        #plt.imshow()
-        #plt.show()
-        cv2.imshow('cv_img', cv_image)
+        cv2.imshow('cv_img', cv_image/2)
+        print ("-------------------------------------------------------------------")
+        print cv_image
+        self.pub.publish(bridge.imgmsg_to_cv2(ros_data, "bgr8"))
         cv2.waitKey(2)
 
 def main(args):
     '''Initializes and cleanup ros node'''
     ic = Image_kinect_depth()
-    rospy.init_node('image', anonymous=True)
+    rospy.init_node('image')
     try:
         rospy.spin()
     except KeyboardInterrupt:
